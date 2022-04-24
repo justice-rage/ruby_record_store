4b0a9fc921d2942e8fe96447534e06589e8e7172
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-24 12:42:41 -0700

    create empty array equates to no albums test

---
+++ spec/album_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require 'album'
+
+describe '#Album' do
+  describe('.all') do
+    it("returns an empty array when there are no albums")
+do
+      expect(Album.all).to(eq([]))
+    end
+  end
+end
