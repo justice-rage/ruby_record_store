dd352310894d7cfc63cac3293a98818a0d96e0c5
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-24 15:40:04 -0700

    rearrange tests

--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -1,6 +1,16 @@
 require 'rspec'
 require 'album'
 
+describe('#save') do
+  it("saves an album") do
+    album = Album.new("Giant Steps", nil) # nil added as second argument
+    album.save()
+    album2 = Album.new("Blue", nil) # nil added as second argument
+    album2.save()
+    expect(Album.all).to(eq([album, album2]))
+  end
+end
+
 describe '#Album' do
   describe('.all') do
     it("returns an empty array when there are no albums") do
@@ -8,15 +18,6 @@ describe '#Album' do
     end
   end
 
-  describe('#save') do
-    it("saves an album") do
-      album = Album.new("Giant Steps", nil) # nil added as second argument
-      album.save()
-      album2 = Album.new("Blue", nil) # nil added as second argument
-      album2.save()
-      expect(Album.all).to(eq([album, album2]))
-    end
-  end
 
   describe('#==') do
     it("is the same album if it has the same attributes as another album") do
