765e2317846b23e2bba422d05097f0fa11645d58
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-24 12:53:58 -0700

    add save method and serialize albums functionality

--- lib/album.rb
+++ lib/album.rb
@@ -1,11 +1,19 @@
 class Album
-  @@albums = {}
+  attr_reader :id, :name #Our new save method will need reader methods.
 
-  def initialize(name)
+  @@albums = {}
+  @@total_rows = 0 # We've added a class variable to keep track of total rows and increment the value when an Album is added.
+
+  def initialize(name, id)
     @name = name
+    @id = id || @@total_rows += 1  # We've added code to handle the id.
   end
 
   def self.all
     @@albums.values()
+  end
+
+  def save
+    @@albums[self.id] = Album.new(self.name, self.id)
   end
 end
