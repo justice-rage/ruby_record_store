047ef09edf30d7211f9620108e6994ac4a6f4500
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-04-24 12:50:29 -0700

    create save test

--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -7,4 +7,14 @@ describe '#Album' do
       expect(Album.all).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it("saves an album") do
+      album = Album.new("Giant Steps", nil) # nil added as second argument
+      album.save()
+      album2 = Album.new("Blue", nil) # nil added as second argument
+      album2.save()
+      expect(Album.all).to(eq([album, album2]))
+    end
+  end
 end
